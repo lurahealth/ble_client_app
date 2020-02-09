667d74de684e78a18566b9d090a7852dd71c8a7f
Rakshak <r.rakshak@gmail.com>
2020-02-09 14:36:11 +0800

    made Secure Storage Util a top level method

--- lib/utils/SecureStorageUtils.dart
+++ lib/utils/SecureStorageUtils.dart
@@ -1,22 +1,19 @@
 import 'package:ble_client_app/utils/StringUtils.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-class SecureStorageUtils{
+writeToSecureStorage(String key, String value) async {
+  await new FlutterSecureStorage().write(key: key, value: value);
+}
 
-  static writeToSecureStorage(String key, String value) async {
-    await new FlutterSecureStorage().write(key: key, value: value);
-  }
+Future<String> readFromSecureStorage(String key) async {
+  return await new FlutterSecureStorage().read(key: key);
+}
 
-  static Future<String> readFromSecureStorage(String key) async {
-    return await new FlutterSecureStorage().read(key: key);
-  }
+deleteFromSecureStorage(String key) async {
+  await new FlutterSecureStorage().delete(key: key);
+}
 
-  static deleteFromSecureStorage(String key) async {
-    await new FlutterSecureStorage().delete(key: key);
-  }
-
-  static saveBLEDeviceName(String deviceName) async{
-    await deleteFromSecureStorage(StringUtils.BLE_DEVICE_NAME);
-    await writeToSecureStorage(StringUtils.BLE_DEVICE_NAME, deviceName);
-  }
+saveBLEDeviceName(String deviceName) async {
+  await deleteFromSecureStorage(StringUtils.BLE_DEVICE_NAME);
+  await writeToSecureStorage(StringUtils.BLE_DEVICE_NAME, deviceName);
 }
