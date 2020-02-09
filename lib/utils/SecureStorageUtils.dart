d0a7d185b2bf10e63726435873a116bc63a7b4a3
Rakshak <r.rakshak@gmail.com>
2020-02-09 10:21:16 +0800

    Added method to save BLE device name to secure sotrage utils

--- lib/utils/SecureStorageUtils.dart
+++ lib/utils/SecureStorageUtils.dart
@@ -1,3 +1,4 @@
+import 'package:ble_client_app/utils/StringUtils.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SecureStorageUtils{
@@ -12,5 +13,10 @@ class SecureStorageUtils{
 
   static deleteFromSecureStorage(String key) async {
     await new FlutterSecureStorage().delete(key: key);
+  }
+
+  static saveBLEDeviceName(String deviceName) async{
+    await deleteFromSecureStorage(StringUtils.BLE_DEVICE_NAME);
+    await writeToSecureStorage(StringUtils.BLE_DEVICE_NAME, deviceName);
   }
 }
